4fd660c9806c88cb920af52b8d1aadde5b8940db
sathvik udupa <sathvikudupa66@gmail.com>
2022-05-25 22:27:23 +0530

    update torgo recipe

--- TORGO_espnet2/asr1/run.sh
+++ TORGO_espnet2/asr1/run.sh
@@ -5,9 +5,22 @@ set -e
 set -u
 set -o pipefail
 
+use_lm=true #do you want to use a language model or not
+asr_config=conf/train_asr_transformer.yaml  #specify acoustic model config
+lm_config=conf/train_lm_transformer.yaml #specify language model config
+train_set=train1  #specify train split
+dev_set=dev1 #specify dev split
+token=char  #char or bpe, with bpe, vary "nbpe" param
+exp_name="default_transformer_model" #specify a custom tag
+
 ./asr.sh \
     --lang en \
-    --token_type "char" \
-    --train_set train1 \
-    --valid_set "dev1 dev2" \
+    --use_lm $use_lm \
+    --token_type $token \
+    --asr_config "${asr_config}" \
+    --lm_config "${lm_config}" \
+    --train_set $train_set \
+    --valid_set $dev_set \
     --test_sets "test1 test2" \
+    --speed_perturb_factors "0.9 1.0 1.1"\
+    --asr_tag $exp_name \
